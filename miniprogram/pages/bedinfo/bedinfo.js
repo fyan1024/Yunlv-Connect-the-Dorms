fa76475398d5d19e9ef32c4acb17d0c672cbfd77
ColdPt <liangboarthur@163.com>
2024-03-07 16:24:26 +0800

    3.7 complete comment

--- miniprogram/pages/bedinfo/bedinfo.js
+++ miniprogram/pages/bedinfo/bedinfo.js
@@ -4,129 +4,146 @@ Page({
   data: {
     goodsDetail: null,
     bedinfo: [],
-    
-    longitude:undefined,
-    latitude:undefined,
-
-    scale:11,
+    show: false,
+    owner_name: '',
+    owner: '',
+    // 以下是留言功能需要的数据
+    content: '', // 用户输入的评论内容
+    user: {},
+    userId: '',
+    userName: '',
+    avatarUrl: '',
+    avatarUrl2: '',
+    bedId: '',
+    comments: [], // 存储查询到的留言数据
+    bed: [],
+    ownerName: ''
+  },
+  showPopup() {
+    this.setData({
+      show: true
+    });
+  },
+  onClose() {
+    this.setData({
+      show: false
+    });
   },
   onLoad(options) {
-    // `bedsId` 的值作为URL参数传递给了 `bedinfo`
-    // `onLoad` 函数可以通过它的参数 `options` 来访问这个传递过来的 `id` 值。
+    const user = wx.getStorageSync('userInfo');
+    console.log("用户信息", user)
+    console.log(user)
+    this.setData({
+      user: user,
+      userId: user._id, // 确保这里的属性名与实际存储的key匹配
+      userName: user.User_name,
+      avatarUrl: user.avatarUrl
+    })
+    console.log("用户id", this.data.userId)
     const bedsId = options.id;
+    // 发起网络请求，获取商品详情数据
     db.collection('Bed').doc(bedsId).get().then(res => {
+        // 处理后台返回的商品信息
         const bedinfo = res.data;
+        // 在页面中渲染商品信息
         this.setData({
-          bedinfo: bedinfo
-        }, () => {
-          // 使用 setData 的回调函数确保数据已被设置
-          console.log("bedinfo页面：", this.data.bedinfo);
-          console.log("开始查询地点 bedinfo.address：", this.data.bedinfo.address);
-          console.log("开始查询大学 bedinfo.university", this.data.bedinfo.university);
-          console.log("开始查询城市 bedinfo.city", this.data.bedinfo.city);
-          this.Univ2Location(this.data.bedinfo.address, this.data.bedinfo.city); 
+          bedinfo: bedinfo,
+          bedId: bedinfo._id
         });
+        console.log("床铺ID", bedinfo._id)
+        this.queryComments(bedinfo._id);
       })
       .catch(error => {
         console.error('获取商品信息失败', error);
-      }
-    );
+      });
+
   },
 
-  /*
-  申请 WebServiceAPI Key，开启腾讯位置服务，参考： 
-    https://lbs.qq.com/faq/serverFaq/webServiceKey
-  分配额度，参考：
-    https://blog.csdn.net/m0_52993798/article/details/133647174
-  配置域名，参考：
-    https://developers.weixin.qq.com/miniprogram/dev/framework/ability/network.html
-  参考腾讯位置服务 demo 实现地址转坐标搜索函数, 地图WebserviceAPI地点搜索接口请求路径及参数，参考：
-    https://lbs.qq.com/service/webService/webServiceGuide/miniprogram?ref=miniprogram_component_map#5
-  例如：
-    // url: 'https://apis.map.qq.com/ws/place/v1/search?page_index=1&page_size=20&boundary=region(北京市,0)&keyword=美食&key=PWHBZ-SREKC-AE52A-A3IEA-QDXLQ-CHB5Q',  
-  */
-
-  Univ2Location(university, city) {
-    var _this = this;
-    var allMarkers = []
-
-    // 在wx.request调用之前声明并初始化cityWithSuffix
-    const cityWithSuffix = city.endsWith("市") ? city : city + "市";
-    
-    wx.request({
-      url: `https://apis.map.qq.com/ws/place/v1/search?page_index=1&page_size=10&boundary=region(${encodeURIComponent(cityWithSuffix)},0)&keyword=${encodeURIComponent(university)}&key=PWHBZ-SREKC-AE52A-A3IEA-QDXLQ-CHB5Q`,
-
-      // header: {
-      //   'content-type': 'application/json' // 默认值
-      // },
-      
-      success(res) {
-        console.log("Loading Univ2Location")
-
-        var result = res.data
-        var pois = result.data
-        console.log(pois)
-        for(var i = 0; i< pois.length; i++){
-          var title = pois[i].title
-          var lat = pois[i].location.lat
-          var lng = pois[i].location.lng
-          console.log(title+","+lat+","+lng)
-          const marker = {
-            id: i,
-            latitude: lat,
-            longitude: lng,
-            width: 50,
-            height: 50,
-            callout: {
-              // 点击marker展示title
-              content: title,
-              display: 'ALWAYS', // 始终显示标题
-              // 其他可选的样式调整，如字体大小、背景颜色等
-              fontSize: 14,
-              bgColor: "#ffffff",
-              padding: 10,
-              borderRadius: 5,
-              borderWidth: 1,
-              borderColor: "#cccccc"
-            }
-          }
-          allMarkers.push(marker);
-        }
-        
-        _this.setData({
-          latitude: allMarkers[0].latitude,
-          longitude: allMarkers[0].longitude,
-          markers: allMarkers
-        })
+  queryComments(bedId) {
+    db.collection('comment').where({
+      bedId: bedId // 确保留言数据中有 bedId 字段关联到床铺
+    }).limit(5).get({
+      success: res => {
+        this.setData({
+          comments: res.data
+        });
       },
-      fail(error) {
-        console.error('地点搜索失败：', error);
+      fail: err => {
+        console.error('获取留言失败', err);
       }
     });
   },
-
-  // 跳转导航APP
-  // https://developers.weixin.qq.com/miniprogram/dev/api/location/wx.openLocation.html
-  onMarkerTap(e) {
-    console.log('Marker ID:', e.markerId);
-    const marker = this.data.markers.find(marker => marker.id === e.markerId);
-    if (marker) {
-      wx.openLocation({
-        latitude: marker.latitude,
-        longitude: marker.longitude,
-        name: marker.callout.content, // 可选，地点名字
-        scale: 18 // 可选，缩放级别，默认为18，范围从5~18
+  handleInput(e) {
+    this.setData({
+      content: e.detail, // 更新页面数据，保存用户输入的评论内容
+    });
+  },
+  submitComment(event) {
+    if (this.data.content.trim() === '') {
+      wx.showToast({
+        title: '留言内容不能为空',
+        icon: 'none',
       });
+      return;
     }
+    const rId = event.currentTarget.dataset.idId;
+    console.log(rId)
+    const db = wx.cloud.database();
+    db.collection('User').where({
+      _id: rId
+    }).get({
+      success: res => {
+        console.log(res.data[0])
+        const temp = res.data[0]
+        console.log(temp.avatarUrl)
+        this.setData({
+          owner: temp
+        });
+        console.log(this.data.owner)
+        this.sub()
+      },
+    });
   },
 
+  sub() {
+    console.log(this.data.owner)
+    db.collection('comment').add({
+      data: {
+        content: this.data.content,
+        createTime: db.serverDate(),
+        userId: this.data.userId,
+        userName: this.data.userName,
+        avatarUrl: this.data.avatarUrl,
+        avatarUrl2: this.data.owner.avatarUrl,
+        bedId: this.data.bedId,
+        receiverId: this.data.bedinfo.ownerId,
+        receiverName: this.data.bedinfo.ownerName,
+      },
+      success: res => {
+        wx.showToast({
+          title: '留言成功',
+          icon: 'success',
+        });
+        // 评论成功后的处理，例如清空输入框，刷新评论列表等
+        this.setData({
+          content: '',
+        });
+        this.queryComments(this.data.bedId); // 重新查询留言
+        // 可以在这里添加获取评论列表的函数调用，以更新显示的评论
+      },
+      fail: err => {
+        wx.showToast({
+          title: '评论失败',
+          icon: 'none',
+        });
+      },
+    });
+  },
 
   onClickIcon1() {
-    wx.showToast({
-      title: '点击图标1',
-      icon: 'none'
+    wx.navigateTo({
+      url: '/pages/chat/chat',
     })
-    console.log
   },
 
   onClickIcon2() {
@@ -144,10 +161,10 @@ Page({
     })
   },
 
-  onClickButton2() {
-    wx.showToast({
-      title: '点击按钮2',
-      icon: 'none'
+  onClickButton2(event) {
+    const bedId = event.currentTarget.dataset.bedid;
+    wx.navigateTo({
+      url: `/pages/reserve/reserve?id=${bedId}`,
     })
   },
 });
