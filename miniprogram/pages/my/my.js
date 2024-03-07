d12d5a4df06c697fc1e797b988c579b5dd2048e3
swiftGrinder <2985499949@qq.com>
2024-03-07 10:06:10 +0800

    update

--- miniprogram/pages/my/my.js
+++ miniprogram/pages/my/my.js
@@ -26,24 +26,29 @@ Page({
     if (userInfo) {
       // 获取云数据库引用
       const db = wx.cloud.database();
-      const usersCollection = db.collection('User'); // 假设你的集合名称为 'User'
 
-      // 查询对应用户名的用户信息
-      usersCollection.where({
-        User_name: userInfo.User_name
-      }).get().then(res => {
-        // 获取查询结果
-        const userData = res.data;
-        if (userData.length > 0) {
-          // 将从云端数据库中获取到的用户信息更新到页面的 user 中
-          this.setData({
-            user: userData[0] // 假设返回的结果为一个数组，取第一个元素作为用户信息
-          });
-        } else {
-          console.error('未找到对应的用户信息');
+      wx.cloud.callFunction({
+        name: "search", //必须和云函数名称一样
+        data: {
+          title: 'User_name',
+          value: userInfo.User_name,
+          name: 'User'
+        },
+        success: (res) => {
+          console.log('请求成功', res)
+          const userData = res.result.data;
+          if (userData.length > 0) {
+            // 将从云端数据库中获取到的用户信息更新到页面的 user 中
+            this.setData({
+              user: userData[0] // 假设返回的结果为一个数组，取第一个元素作为用户信息
+            });
+          } else {
+            console.error('未找到对应的用户信息');
+          }
+        },
+        fail(res) {
+          console.log('请求错误', res)
         }
-      }).catch(err => {
-        console.error('查询用户信息失败', err);
       });
     } else {
       console.error('未找到本地缓存的用户信息');
@@ -122,14 +127,6 @@ Page({
     console.log(userId)
     wx.navigateTo({
       url: `/pages/order/order?id=${userId}`,
-    });
-  },
-
-  goToComment(event) {
-    const userId = event.currentTarget.dataset.user2Id;
-    console.log(userId)
-    wx.navigateTo({
-      url: `/pages/comment/comment?id=${userId}`,
     });
   },
 
--- miniprogram/pages/upload1/upload1.js
+++ miniprogram/pages/upload1/upload1.js
@@ -1,20 +1,9 @@
 // pages/upload/upload.js
 // import Toast from '@vant/weapp/toast/toast';
-const app = getApp();
 let db = wx.cloud.database() //操作数据库
-let genderArray = ['男','女'];
 Page({
   data: {
-<<<<<<< Updated upstream
-    city: '',
-=======
-    array: genderArray,//引入性别选择
-    gender:'',
-    genderValue:'',
-    imageArray:[],//上传多张图片
-    // imgList: [],
     City: '',
->>>>>>> Stashed changes
     university: '',
     address: '',
     announcements: '',
@@ -24,11 +13,10 @@ Page({
     dormitory_num: '',
     likes_num: '0',
     text: '',
-    ownerId:'',
     show: false,
     pic: false,
-    minDate: new Date().getTime(),
-    maxDate: '',
+    minDate: new Date(2024, 2, 28).getTime(),
+    maxDate: new Date(2024, 3, 31).getTime(),
     bedUrl: "https://img0.baidu.com/it/u=1429435380,946942033&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=500",
     user: '',
     fileList: [
@@ -41,96 +29,7 @@ Page({
     ],
     recordId: '',
     fileID: '',
-<<<<<<< Updated upstream
-
-    // 地址 Locater
-    showLocateSearchPicker: false,
-    searchLocaeResults: [],
-    selectedLocateOption: null,
-    longitude:undefined,
-    latitude:undefined,
   },
-
-  /* 以下地址选择器 */
-  // Function to initiate the university search based on city and university
-  initiateLocateSearch: function() {
-    const { city, university } = this.data;
-    if (city && university) {
-      this.CityAndUniv2Location(university, city);
-    } else {
-      wx.showToast({
-        title: 'Please enter both city and university',
-        icon: 'none',
-      });
-    }
-=======
-    icon:{
-      "width":"100rpx",
-      "height":"100rpx",
-      "path":""
-    }
-  },
-  bindPickerChange: function(e) {
-    console.log('picker发送选择改变，携带值为', e.detail.value)
-    this.setData({
-      gender: genderArray[e.detail.value],
-      genderValue: e.detail.value
-    })
->>>>>>> Stashed changes
-  },
-  // Function to perform the location search
-  CityAndUniv2Location: function(university, city) {
-    const cityWithSuffix = city.endsWith("市") ? city : city + "市";
-    wx.request({
-      url: `https://apis.map.qq.com/ws/place/v1/search?page_index=1&page_size=10&boundary=region(${encodeURIComponent(cityWithSuffix)},0)&keyword=${encodeURIComponent(university)}&key=PWHBZ-SREKC-AE52A-A3IEA-QDXLQ-CHB5Q`,
-      success: (res) => {
-        const searchResults = res.data.data.map((item, index) => ({
-          id: index,
-          title: item.title,
-          lat: item.location.lat,
-          lng: item.location.lng,
-        }));
-        this.setData({
-          searchResults: searchResults,
-          showLocateSearchPicker: true,
-        });
-        console.log('search results saving to searchResults:  ', searchResults)
-      },
-      fail: (error) => {
-        console.error('Search failed:', error);
-        wx.showToast({
-          title: 'Search failed, please try again',
-          icon: 'none',
-        });
-      }
-    });
-  },
-  onLocateSelectOption: function(e) {
-    const selectedId = e.currentTarget.dataset.id;
-    const selectedOption = this.data.searchResults.find(option => option.id === selectedId);
-    this.setData({
-      selectedLocateOption: selectedOption, // Save the selected option
-      showLocateSearchPicker: false, // Close the popup after selection
-      address: selectedOption.title,
-      latitude: selectedOption.lat, // Set latitude
-      longitude: selectedOption.lng, // Set longitude
-    });
-    console.log('Selected location:', this.data.selectedLocateOption);
-    // Additional logic to use the selected location can follow here
-    wx.showToast({
-      title: '成功保存地址',
-      icon: 'success',
-      duration: 1500
-    });
-  },
-  onLocateClosePopup: function() {
-    this.setData({
-      showUnivSearchPopup: false,
-    });
-  },
-  /* 地址选择器部分结束 */
-
-
   /**
   * 生命周期函数--监听页面加载
   */
@@ -138,14 +37,6 @@ Page({
     this.setData({
       user: wx.getStorageSync('userInfo')
     })
-    this.calculateMaxDate();
-  },
-  calculateMaxDate() {
-    const today = new Date();
-    const twoMonthsLater = new Date(today.getFullYear(), today.getMonth() + 2, today.getDate());
-    this.setData({
-      maxDate: twoMonthsLater.getTime()
-    });
   },
   onDisplay() {
     this.setData({ show: true });
@@ -159,15 +50,10 @@ Page({
   },
   onConfirm(event) {
     const [start, end] = event.detail;
-    const startDate = this.formatDate(start); // 使用已有的 formatDate 方法或自定义格式
-    const endDate = this.formatDate(end);
     this.setData({
       show: false,
-      start_time: startDate,
-      deadline: endDate,
-      date: `${startDate} - ${endDate}`, // 更新页面上显示的日期范围
+      date: `${this.formatDate(start)} - ${this.formatDate(end)}`,
     });
-    console.log(this.data.deadline)
   },
   onChange(e) {
     console.log(e.detail); // 打印输入的内容看看
@@ -181,16 +67,10 @@ Page({
   },
   uploadcontent: function () {
     let that = this;
-    const db = wx.cloud.database();
-
-    // city university address announcements bed_num deadline start_time 
-    // dormitory_num likes_num text ownerId
-    
-    // 解构页面数据
-    const { city, university, address, bed_num, announcements, dormitory_num, pic, start_time, deadline, latitude, longitude } = this.data;
-
-    // 检查必填信息是否已填写
-    if (!city || !university || !address || !bed_num || !dormitory_num) {
+    const db = wx.cloud.database(); // 获取数据库引用
+    const { university, address, bed_num, announcements, dormitory_num, pic } = this.data;
+    // 检查schoolName是否为空
+    if (!university || !address || !bed_num || !dormitory_num) {
       wx.showToast({
         title: '请填写完整信息',
         icon: 'none',
@@ -204,43 +84,33 @@ Page({
       });
       return;
     }
-    // 向Bed集合添加记录，包括开始和结束日期
+    // 向Bed集合添加记录
     db.collection('Bed').add({
       data: {
-        city: city,
+        city: this.data.City,
         Is_busy: false,
         address: address,
         announcements: announcements,
         bed_num: bed_num,
-        start_time: start_time, // 包含开始日期
-        deadline: deadline, // 包含结束日期
+        deadline: '',
+        start_time: '',
         dormitory_num: dormitory_num,
         evaluation: '',
-        likes_num: 0,
+        likes_num: '0',
         picture_add: that.data.bedUrl,
         university: university,
         createTime: db.serverDate(), // 服务器时间
-        ownerId:that.data.user._id,
-        ownerName:that.data.user.User_name,
-        longitude: longitude,
-        latitude: latitude,
       },
       success: res => {
         // 添加成功后的处理
         wx.showToast({
           title: '上传成功',
           icon: 'success',
-          duration: 1500
         });
         console.log('上传成功，记录 _id: ', res._id);
         this.setData({
           recordId: res._id // 保存记录ID
         });
-        setTimeout(() => {
-          wx.navigateBack({
-            delta: 1 // 返回上一级页面，如果当前页面是通过 navigateTo 打开的，delta 设置为 1 即可
-          });
-        }, 1500); // 这里的延时应与 showToast 的 duration 相匹配
         console.log(this.data.fileID)
         this.updateDatabase(this.data.fileID);
       },
