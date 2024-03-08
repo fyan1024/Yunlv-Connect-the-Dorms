c52fd261d58bb937dd2f28d7123c0baf0ed5a8f5
fyan1024 <frankxiaoluyan@gmail.com>
2024-03-08 10:01:06 +0800

    update addReserve

--- miniprogram/pages/bedinfo/bedinfo.js
+++ miniprogram/pages/bedinfo/bedinfo.js
@@ -5,28 +5,110 @@ Page({
     goodsDetail: null,
     bedinfo: [],
     
+    /* map 部分 */
     longitude:undefined,
     latitude:undefined,
-
     scale:11,
+    /* map 部分结束 */
+
+    /* 留言 部分 */
+    content: '', // 用户输入的评论内容
+    user: {},
+    userId: '',
+    userName: '',
+    avatarUrl: '',
+    bedId: '',
+    comments: [], // 存储查询到的留言数据
+    /* 留言 部分结束 */
+
   },
   onLoad(options) {
+    /* 留言功能 */
+    const user = wx.getStorageSync('userInfo');
+    console.log("用户信息", user)
+    this.setData({
+      user: user,
+      userId: user._id, // 确保这里的属性名与实际存储的key匹配
+      userName: user.name,
+      avatarUrl: user.avatarUrl
+    })
+    console.log("用户id", this.data.userId)
+    /* 留言功能结束 */
     // `bedsId` 的值作为URL参数传递给了 `bedinfo`
     // `onLoad` 函数可以通过它的参数 `options` 来访问这个传递过来的 `id` 值。
     const bedsId = options.id;
     db.collection('Bed').doc(bedsId).get().then(res => {
         const bedinfo = res.data;
         this.setData({
-          bedinfo: bedinfo
+          bedinfo: bedinfo,
+          bedId: bedinfo._id
         }, () => {
           // 使用 setData 的回调函数确保数据已被设置
           this.Univ2Location(); 
         });
-      })
-      .catch(error => {
+        console.log("床铺ID", bedinfo._id)
+        this.queryComments(bedinfo._id);
+      }).catch(error => {
         console.error('获取商品信息失败', error);
       }
     );
+  },
+  queryComments(bedId) {
+    db.collection('comment').where({
+      bedId: bedId // 确保留言数据中有 bedId 字段关联到床铺
+    }).get({
+      success: res => {
+        this.setData({
+          comments: res.data
+        });
+      },
+      fail: err => {
+        console.error('获取留言失败', err);
+      }
+    });
+  },
+  handleInput(e) {
+    this.setData({
+      content: e.detail, // 更新页面数据，保存用户输入的评论内容
+    });
+  },
+  submitComment() {
+    if (this.data.content.trim() === '') {
+      wx.showToast({
+        title: '留言内容不能为空',
+        icon: 'none',
+      });
+      return;
+    }
+    const db = wx.cloud.database();
+    db.collection('comment').add({
+      data: {
+        content: this.data.content,
+        createTime: db.serverDate(),
+        userId: this.data.userId,
+        userName: this.data.userName,
+        avatarUrl: this.data.avatarUrl,
+        bedId: this.data.bedId
+      },
+      success: res => {
+        wx.showToast({
+          title: '留言成功',
+          icon: 'success',
+        });
+        // 评论成功后的处理，例如清空输入框，刷新评论列表等
+        this.setData({
+          content: '',
+        });
+        this.queryComments(this.data.bedId); // 重新查询留言
+        // 可以在这里添加获取评论列表的函数调用，以更新显示的评论
+      },
+      fail: err => {
+        wx.showToast({
+          title: '评论失败',
+          icon: 'none',
+        });
+      },
+    });
   },
 
   /*
